ebe3fc39418b49c3269ff45392ce0b210d94a81a
Josip Digital <info@digriot.com>
2017-02-11 22:04:51 +0100

    Add project files.

---
+++ mousepos/Scripts/app.js
@@ -0,0 +1,10 @@
+﻿var app = angular.module('mouse', [])
+app.controller('mouseposition', function($scope){
+    $scope.doClick = function (event) {
+
+        var x = event.x;
+        var y = event.y;
+        $scope.offsetX = event.originalEvent.layerX;
+        $scope.offsetY = event.originalEvent.layerY;
+    };
+});
